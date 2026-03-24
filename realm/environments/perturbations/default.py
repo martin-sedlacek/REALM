b5e398f0c8cec82f11bdd422025557835ac1e0fc
martin-sedlacek <martin.martin.sedlacek@gmail.com>
2026-03-24 18:03:06 +0100

    restructure

---
+++ realm/environments/perturbations/default.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from realm.environments.realm_environment_dynamic import RealmEnvironmentDynamic
+
+
+def default(env: "RealmEnvironmentDynamic") -> None:
+    return
